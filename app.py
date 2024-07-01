aac59f71633d99a204a144c3c7e7c5da07465456
Sowmya P V <167305660+sowmya-p-v@users.noreply.github.com>
2024-07-01 14:41:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,10 +152,12 @@ if selected == 'Investment Risk Prediction':
         sentiment_map = {0: -1, 1: 0, 2: 1}
         predicted_sentiment = sentiment_map[predicted_class]
 
-        extra_features = np.array([[credit_rating_impact, predicted_sentiment]])
+        credit_rating_impact_array = np.array([credit_rating_impact])
+        predicted_sentiment_array = np.array([predicted_sentiment])
         scaled_inputs_trimmed = scaled_inputs[:, :-4]
 
-        input_with_predictions = np.concatenate(([credit_rating_impact], scaled_inputs_trimmed.flatten(), [predicted_sentiment]))
+        input_with_predictions = np.concatenate((credit_rating_impact_array, scaled_inputs_trimmed.flatten(), predicted_sentiment_array))
+        input_with_predictions = input_with_predictions.reshape(1, -1)
 
         # input_with_predictions = np.concatenate((input_vector, np.array([[credit_rating_impact, predicted_sentiment]])), axis=1)
 
