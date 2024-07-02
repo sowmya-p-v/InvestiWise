946fbcfc0a423d1e42bd2c5ce0118aedbfb7c6d8
Sowmya P V <167305660+sowmya-p-v@users.noreply.github.com>
2024-07-02 21:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -197,20 +197,20 @@ elif selected == 'Data Viewer':
     
 
     df_subset = df.sample(n=1000, random_state=42)
-    df_subset = df.sample(n=1000, random_state=42)
+    # df_subset = df.sample(n=1000, random_state=42)
   
-    company = st.sidebar.selectbox('Select Company (optional)', ['None'] + list(df_subset['COMPANY'].unique()))
+    company = st.sidebar.selectbox('Select Company (optional)', ['None'] + list(df_subset['Company'].unique()))
     if company != 'None':
-       filtered_df = df_subset[df_subset['COMPANY'] == company]
+       filtered_df = df_subset[df_subset['Company'] == company]
     else:
-       market = st.sidebar.multiselect('Select Market', df_subset['MARKET'].unique(), df_subset['MARKET'].unique())
-       sector = st.sidebar.multiselect('Select Sector', df_subset['SECTOR'].unique(), df_subset['SECTOR'].unique())
+       market = st.sidebar.multiselect('Select Market', df_subset['Market'].unique(), df_subset['Market'].unique())
+       sector = st.sidebar.multiselect('Select Sector', df_subset['Sector'].unique(), df_subset['Sector'].unique())
     filtered_df = df_subset[
-    (df_subset['MARKET'].isin(market)) &
-    (df_subset['SECTOR'].isin(sector))
+    (df_subset['Market'].isin(market)) &
+    (df_subset['Sector'].isin(sector))
           ]
     if not filtered_df.empty:
-       st.write(filtered_df[['COMPANY', 'MARKET', 'SECTOR', 'COUNTRY_MARKET_RISK_RETURN', 
+       st.write(filtered_df[['Company', 'Market', 'Sector', 'COUNTRY_MARKET_RISK_RETURN', 
                                          'COUNTRY_RFR', 'COUNTRY_RISK_PREMIUM','GROSS_MARGIN','OPER_MARGIN','EPS_GROWTH',
                                          'UNLEVERED_BETA','WACC','Credit rating impact',
                                           'TOTAL_E', 'TOTAL_S', 'TOTAL_G']])
